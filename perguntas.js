5bcae7600bbf7c9ebc0c7878bcf866ba10fdd6bc
JoaoCaldeira10 <160407845+JoaoCaldeira10@users.noreply.github.com>
2024-12-09 13:45:47 -0300

    arquivos

---
+++ perguntas.js
@@ -0,0 +1,89 @@
+criaCartao(
+    'JOGOS',
+    'Quais irmãos encanadores estrelam a franquia Super Mario da Nintendo?',
+    'Mário e Luigi'
+)
+
+criaCartao(
+    'JOGOS',
+    'Qual jogo de terror desafia os jogadores com animatrônicos?',
+    'Five nights at Freddys'
+)
+
+criaCartao(
+    'JOGOS',
+    'Qual herói da Sony é extremamente protetor com a figura de sua filha adotiva?',
+    'Joel(The Last of Us)'
+)
+
+criaCartao(
+    'MARVEL',
+    'Em que ano foi lançado o primeiro filme do Homem de Ferro, dando início ao Universo Cinematográfico da Marvel?',
+    '2008'
+)
+
+criaCartao(
+    'MARVEL',
+    'Qual é o filme da Marvel de menor bilheteria em 2023?',
+    'As Marvels'
+)
+
+criaCartao(
+    'MARVEL',
+    ' Em que cena pós-créditos Thanos apareceu pela primeira vez?',
+    'Os Vingadores'
+)
+
+criaCartao(
+    'ARCANE',
+    'Qual personagem tem o poder de voltar 4 segundos no tempo?',
+    'Ekko'
+)
+
+criaCartao(
+    'ARCANE',
+    ' Quais as irmãs que protagonizam a seríe baseada no universo de League of Legends?',
+    'Vi e Jinx'
+)
+
+criaCartao(
+    'ARCANE',
+    'Qual o nome da tecnologia inventada e usada nas armas da série?',
+    'Hextech'
+)
+
+criaCartao(
+    'GOD OF WAR',
+    'Qual o nome da serpente que aparece na saga Nórdica?',
+    'Jörmungandr'
+)
+
+criaCartao(
+    'GOD OF WAR',
+    'Quais as armas que Kratos possui em God of War Ragnarok?',
+    'Lâminas do Caos, Leviatã e Lança Draupnir'
+)
+
+criaCartao(
+    'GOD OF WAR',
+    'Qual o nome do cara que pulou do barco do Frey?',
+    'Birgir'
+)
+
+criaCartao(
+    'FORTNITE',
+    'Quem lidera o grupo denominado os sete?',
+    'Fundação'
+)
+
+criaCartao(
+    'FORTNITE',
+    'Qual o meio utilizado na história do jogo para justificar as parcerias?',
+    'Ponto Zero'
+)
+
+criaCartao(
+    'FORTNITE',
+    'Quais foram os novos modos inplementados no Metaverso do Fortnite?',
+    'Lego Fortnite, Rocket Racing, Festival e Raiz'
+)
